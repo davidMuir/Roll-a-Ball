6a034f160027c024555c6cb7ffcd93cdd6c08aa4
David Muir <hey@davidmuir.co>
2016-10-23 00:38:35 +0100

    First commit, and it liiiiives

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+
+	public GameObject Player;
+	private Vector3 _offset;
+
+	public void Start()
+	{
+		_offset = transform.position - Player.transform.position;
+	}
+
+	public void LateUpdate()
+	{
+		transform.position = Player.transform.position + _offset;
+		transform.LookAt(Player.transform);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float Speed;
+	private Rigidbody _rigidBody;
+	private int _count = 0;
+	public Text CountText;
+	public Text WinText;
+
+	public void Start()
+	{
+		_rigidBody = GetComponent<Rigidbody>();
+		UpdateCountText();
+		WinText.text = string.Empty;
+	}
+
+	public void FixedUpdate()
+	{
+		var moveHorizontal = Input.GetAxis("Horizontal");
+		var moveVertical = Input.GetAxis("Vertical");
+
+		var movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+		_rigidBody.AddForce(movement * Speed);
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if (!other.gameObject.CompareTag("Pickup")) return;
+
+		other.gameObject.SetActive(false);
+		_count++;
+		UpdateCountText();
+	}
+
+	private void UpdateCountText()
+	{
+		var remaining = GameObject.FindGameObjectsWithTag("Pickup").Length; 
+
+		CountText.text = $"Count: {_count}";
+		
+		if (remaining > 0) return;
+
+		WinText.text = "You Win!";
+	}
+}
